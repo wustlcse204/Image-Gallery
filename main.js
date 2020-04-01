050746d74ad0943c7d7b89d7192a9006af7eec31
nickblake <nicholasblake@wustl.edu>
2020-03-31 23:21:32 -0500

    hi
    purpol

--- main.js
+++ main.js
@@ -1,32 +1,13 @@
- function make_big() {
-   var img = this. firstElementChild;
+ const openimage = document.createElement('div')
+ openimage.id= 'openimage'
+ document.body.appendChild(lightbox)
 
-   varbigImg = document.createElement("img");
-   bigImg.src = img.src;
-
-   document.getElementById("zoomed").appendChild(bigImg);
-
-
-   document.getElementById("zoomed").classList.remove("hidden");
- }
-
- function close(){
-  console.log("CLOSE!");
-  document.getElementById("zoomed").classList.add("hidden");
- }
-
- var imges = document.getElemetsByClassName("small");
- for (i = 0; i < images.length; i++) {
-   var currentImage = images [i];
-   currentImage.addEventListener("click", make_big);
- }
-
- var closeButton = document.getElementById("close");
- closeButton.addEventListener("click", close);
-
-}
-
-//w3 schools
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+ const images = document.querySelectorAll('img')
+ images.forEach(image =>){
+   image.addEventListener('click, e=>'){
+     lightbox.classList.add('active')
+     const img = document.createElement('img')
+     img.src = image.src
+     lightbox.appendChild(img)
+   })
+ })
